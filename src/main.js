7036dd6f6c2be4b9f7b7d708019c2a451942a369
Matheus <matheuspl92@hotmail.com>
2022-08-25 19:52:41 -0300

    Update HTML elements and CSS styling

--- src/main.js
+++ src/main.js
@@ -5,12 +5,10 @@ import getData from './scripts/getData.js';
 // API KEYS
 const OPEN_WEATHER_KEY = '6cae0cb3d6b3ff316c8db6713f40ca12';
 
-const searchBox = document.getElementById('search-box');
+const contentBox = document.getElementById('content-box');
 const searchForm = document.getElementsByTagName('form')[0];
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
-
-const weatherDataBox = document.getElementById('weather-data-box');
 
 searchForm.addEventListener('submit', () => {
   const formData = new FormData(searchForm);
