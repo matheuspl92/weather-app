c9d6ef7935dc50f66ff43f64b6a62230599b0c95
Matheus <matheuspl92@hotmail.com>
2022-08-26 20:43:47 -0300

    Fix API request

--- src/scripts/getGeocodeData.js
+++ src/scripts/getGeocodeData.js
@@ -1,7 +1,7 @@
 /* eslint-disable consistent-return */
 const getGeocodeData = async (searchString, key) => {
   try {
-    const geocodeResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchString}&limit=1&appid=${key}`, { mode: 'cors' });
+    const geocodeResponse = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${searchString}&limit=1&appid=${key}`, { mode: 'cors' });
     const geocodeData = await geocodeResponse.json();
     return geocodeData[0];
   } catch (error) {
